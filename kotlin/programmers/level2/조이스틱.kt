1085684a05f7f12bbbe0edde08eb1b8a75032fd4
ChangXXX <eqqt97@naver.com>
2022-06-17 16:38:48 +0900

    solve 프로그래머스 조이스틱

--- kotlin/programmers/level2/조이스틱.kt
+++ kotlin/programmers/level2/조이스틱.kt
@@ -1,29 +1,72 @@
-//import java.lang.Integer.min
-//
-//fun solution(name: String): Int {
-//    var answer = 0
-//    val minList =  MutableList<Int>(name.length){0}
-//    var minMove = name.length - 1
-//
-//    name.toCharArray().forEachIndexed { index, c ->
-//        minList[index] = Math.min('Z' - c + 1, c - 'A') // 상하 이동횟수
-//    }
-//
-//    for(i in name.indices){ // 좌우 이동
-//        if(name[i] != 'A'){
-//            var aPos = i + 1;
-//            while(aPos < name.length && name[aPos] == 'A'){
-//                aPos += 1
-//            }
-//            val move = 2 * i + name.length - aPos
-//            minMove = min(move, minMove)
-//        }
-//    }
-//    answer = minList.sum() + minMove
-//
-//    return answer
-//}
-//
-//fun main(args:Array<String>){
-//    print(solution("BBBAAB"))
-//}
+import kotlin.math.min
+
+/*
+ *  programmers 조이스틱 https://programmers.co.kr/learn/courses/30/lessons/42860
+*/
+
+class 조이스틱 {
+    var moveCount = 99999
+
+    fun solution(name: String): Int {
+        var answer = 0
+        val moves = IntArray(name.length) { 0 }
+        val check = BooleanArray(name.length) { false }
+
+        for (i in name.indices) {
+            moves[i] = min(name[i] - 'A', 'Z' - name[i] + 1)
+            if(name[i] == 'A') check[i] = true
+        }
+
+        if (name.all { it == 'A' }) return answer
+
+        check[0] = true
+        dfs(check, 0, 0, name)
+
+        answer = moves.sum() + moveCount
+
+        return answer
+    }
+
+    fun dfs(check: BooleanArray, cnt: Int, idx: Int, name: String) {
+        if (check.all { it == true }) {
+            if (moveCount > cnt) moveCount = cnt
+            return
+        }
+
+        if (cnt >= moveCount) return
+
+        var right = idx
+        var rightCount = 0
+        while(true){
+            if(right == name.length - 1) right = 0
+            else right += 1
+            rightCount += 1
+
+            if(!check[right] && name[right] != 'A') break
+        }
+
+        var left = idx
+        var leftCount = 0
+        while(true){
+            if(left == 0) left = name.length - 1
+            else left -= 1
+            leftCount += 1
+
+            if(!check[left] && name[left] != 'A') break
+        }
+
+        check[right]= true
+        dfs(check, cnt + rightCount, right, name)
+        check[right] = false
+
+        check[left] = true
+        dfs(check, cnt + leftCount, left, name)
+        check[left] = false
+    }
+}
+
+fun main(args: Array<String>) {
+    val c = 조이스틱()
+
+    print(c.solution("BBABAAAABBBAAAABABB"))
+}
